f3129f30a04b5bf49b57f19c56c06879c5747198
Administrator <Administrator@WIN-TDBCVHFGPTF>
2019-06-03 09:23:56 +0800

    ok 1

--- test/src/test/syh.java
+++ test/src/test/syh.java
@@ -5,6 +5,8 @@ public class syh {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello,github.");
+		System.out.println("hello,github.");
+		
 	}
 
 }
